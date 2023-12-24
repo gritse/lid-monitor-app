d4fef783e11482c47cb84c242178fdb246617271
gritse <24323376+gritse@users.noreply.github.com>
2023-12-24 16:10:23 +0100

    Initial commit

---
+++ LidMonitorApp/PowerCfg.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LidMonitorApp
+{
+    public static class PowerCfg
+    {
+        public enum LidAction
+        {
+            Nothing = 0,
+            Sleep = 1
+        }
+        
+        public static void SetLidAction(LidAction action)
+        {
+            RunPowerCfg($"/setacvalueindex SCHEME_CURRENT SUB_BUTTONS LIDACTION {(int)action}");
+            RunPowerCfg($"/setdcvalueindex SCHEME_CURRENT SUB_BUTTONS LIDACTION {(int)action}");
+            RunPowerCfg("-setactive SCHEME_CURRENT");
+        }
+        
+        public static LidAction GetLidAction()
+        {
+            var output = RunPowerCfg("/query SCHEME_CURRENT SUB_BUTTONS LIDACTION");
+            return (LidAction)int.Parse(Regex.Match(output, @"(?<=0x)\d+").Value);
+        }
+        
+        public static string RunPowerCfg(string parameters)
+        {
+            // Create process start info
+            var processStartInfo = new ProcessStartInfo
+            {
+                FileName = "powercfg.exe",
+                Arguments = parameters,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,            
+                StandardOutputEncoding = Encoding.GetEncoding(866)
+            };
+
+            // Create process
+            using (var process = new Process())
+            {
+                process.StartInfo = processStartInfo;
+                process.Start();
+
+                // Capture output
+                var output =  process.StandardOutput.ReadToEnd();
+                var error = process.StandardError.ReadToEnd();
+
+                // Wait for the process to exit
+                process.WaitForExit();
+
+                // Check for errors
+                if (!string.IsNullOrEmpty(error))
+                {
+                    throw new Exception($"Error executing powercfg: {error}");
+                }
+
+                return output;
+            }
+        }
+    }
+}
---
+++ LidMonitorApp/TrayApplicationContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+using LidMonitorApp.Properties;
+using Microsoft.Win32;
+
+namespace LidMonitorApp
+{
+    public class TrayApplicationContext : ApplicationContext
+    {
+        private readonly NotifyIcon _trayIcon;
+
+        public TrayApplicationContext()
+        {
+            // Initialize Tray Icon
+            _trayIcon = new NotifyIcon()
+            {
+                Icon = Resources.TrayIcon,
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                    new MenuItem("Exit", Exit)
+                }),
+                Visible = true
+            };
+
+            SystemEventsOnDisplaySettingsChanged(default, default);
+            SystemEvents.DisplaySettingsChanged += SystemEventsOnDisplaySettingsChanged;
+        }
+
+        private void SystemEventsOnDisplaySettingsChanged(object sender, EventArgs e)
+        {
+            PowerCfg.SetLidAction(Screen.AllScreens.Length >= 2
+                ? PowerCfg.LidAction.Nothing
+                : PowerCfg.LidAction.Sleep);
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            _trayIcon.Visible = false;
+            Application.Exit();
+        }
+    }
+}
